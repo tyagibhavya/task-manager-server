b6238ba4d367fc9a7f65d7c86f2d847793409b28
Bhavya Tyagi <99269005+tyagibhavya@users.noreply.github.com>
2023-04-05 11:51:53 +0530

    Changed Origin
    [process.env.FRONTEND_URL] => 'process.env.FRONTEND_URL'

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: [process.env.FRONTEND_URL],
+        origin: 'process.env.FRONTEND_URL',
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         credentials: true,
     })
